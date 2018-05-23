4e047d599e6c7b928995166ebe8e73f5cacfae0e
Christoph Franke <krito@krito.de>
2018-05-23 23:10:29 +0200

    added complete boot scene with animations

--- src/js/heizer.js
+++ src/js/heizer.js
@@ -10,22 +10,74 @@ const createHeizer = async () => {
   const heizerTexture = new THREE.TextureLoader().load(`/HeizerKoerperCompleteMap.png`)
   const material = new THREE.MeshBasicMaterial({ map: heizerTexture })
 
-  return new THREE.Mesh( geometry, material );
+  return new THREE.Mesh( geometry, material )
+}
+
+const createBoot = async () => {
+  const obj = await new Promise(resolve => {
+    const loader = new THREE.ObjectLoader()
+    loader.load(`/bootMitSzene.json`, obj => {
+      resolve(obj)
+    })
+  })
+  const boot = obj.children[0]
+  const bootTexture = new THREE.TextureLoader().load('/SchiffCompleteMap.png')
+  boot.material = new THREE.MeshBasicMaterial({ map: bootTexture })
+  return [boot, obj.animations[0]]
+  // const bootTexture = new THREE.TextureLoader().load(`/SchiffCompleteMap.png`)
+  // const material = new THREE.MeshBasicMaterial({ map: bootTexture })
+  // return new THREE.Mesh( geometry, material )
+}
+
+const createAnimations = (scene) => {
+  // Create an AnimationMixer, and get the list of AnimationClip instances
+  const mixer = new THREE.AnimationMixer( scene )
+  const action = mixer.clipAction( scene.animations[0] )
+  action.play()
+
+  return mixer
+}
+
+const createBootSzene = async () => {
+  const obj = await new Promise(resolve => {
+    const loader = new THREE.ObjectLoader()
+    loader.load(`/bootSzene.json`, obj => {
+      resolve(obj)
+    })
+  })
+  console.log(obj)
+  const bootSzeneTexture = new THREE.TextureLoader().load('/schiffDiffuseMap.png')
+  const material = new THREE.MeshBasicMaterial({ map: bootSzeneTexture })
+  obj.children.forEach(mesh => {
+    mesh.material = material
+  })
+
+  return obj
+  // const boot = obj.children[0]
+  // return [boot, obj.animations[0]]
 }
 
 
 export default async () => {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+  // const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 )
 
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize( window.innerWidth, window.innerHeight )
+  document.body.appendChild( renderer.domElement )
 
-  const heizer = await createHeizer();
-  scene.add( heizer );
+  const heizer = await createHeizer()
+  // const [boot, clip] = await createBoot()
+  const scene = await createBootSzene()
+  scene.add(heizer)
+  const mixer = createAnimations(scene)
+  // scene.add( boot )
+  // scene.add( heizer )
 
-  camera.position.z = 5;
+  // camera.position.z = 5
+  camera.position.x = 8.146366342379562
+  camera.position.y = 3.227381223127345
+  camera.position.z = 9.682811196058351
 
   const captureCamera = (e) => {
     const alpha = 0.001
@@ -49,10 +101,20 @@ export default async () => {
     }
   })
 
-  const animate = function () {
-    requestAnimationFrame( animate );
-    renderer.render(scene, camera);
-  };
+  let deltaSeconds = 0
+  let lastTime = 0
+  const animate = function (time) {
+    if (lastTime) {
+      deltaSeconds = (time - lastTime) / 1000
+    } else {
+      deltaSeconds = 0
+    }
+    lastTime = time
+    mixer.update( deltaSeconds )
+    renderer.render(scene, camera)
 
-  animate();
+    requestAnimationFrame( animate )
+  }
+
+  animate()
 }
