1602b8f9de832255bc48b2a1a124ae12c577f1fa
Christoph Franke <krito@krito.de>
2018-05-24 00:49:55 +0200

    added different scroll behavior for header items

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,19 @@ const initializeNav = () => {
 			});
 		})
 	})
+
+	const brand = document.querySelector('.brand')
+	const nav = document.querySelector('nav')
+	const boot = document.querySelector('#boot')
+	window.addEventListener('scroll', () => {
+		if (window.scrollY - 50 >= boot.getBoundingClientRect().bottom) {
+			brand.classList.add('invisible')
+			nav.classList.add('background')
+		} else {
+			brand.classList.remove('invisible')
+			nav.classList.remove('background')
+		}
+	})
 }
 
 initializeNav();
