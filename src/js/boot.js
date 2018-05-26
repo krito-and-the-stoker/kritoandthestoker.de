5e3cee6da4e20f4cf65c1718af67d46dff9e6a87
Christoph Franke <krito@krito.de>
2018-05-26 21:13:10 +0200

    refactored boot.js and put all the functionality into util.js

--- src/js/boot.js
+++ src/js/boot.js
@@ -1,118 +1,50 @@
 import * as THREE from 'three'
-
-const skyColor = 0xdde8fd
-const groundColor = 0xd1daed
-const hemisPhereIntensity = 1.1
-
-const directionalColor = 0xfaca8c
-const directionalIntensity = 1.0
-const directionalLightPosition = {
-  x: 1,
-  y: 1,
-  z: -3,
-}
-
-
-const createBootSzene = async () => {
-  const scene = await new Promise(resolve => {
-    const loader = new THREE.ObjectLoader()
-    loader.load(`/assets3d/bootSzene.json`, scene => {
-      resolve(scene)
-    })
-  })
-  const bootSzeneTexture = new THREE.TextureLoader().load('/assets3d/Atlas_color.png')
-  const bootSzeneAO = new THREE.TextureLoader().load('/assets3d/SchiffSzene_AO.jpg')
-  const bootSzeneRoughness = new THREE.TextureLoader().load('/assets3d/Atlas_roughness.png')
-  const bootEnvironment = new THREE.CubeTextureLoader()
-    .setPath('/assets3d/cubemaps/bluesky/')
-    .load([
-      'posX.jpg',
-      'negX.jpg',
-      'posY.jpg',
-      'negY.jpg',
-      'posZ.jpg',
-      'negZ.jpg'
-    ])
-
-  const material = new THREE.MeshStandardMaterial({
-    map: bootSzeneTexture,
-    aoMap: bootSzeneAO,
-    roughnessMap: bootSzeneRoughness,
-    roughness: 0.8,
-    aoMapIntensity: 1.2,
-    envMap: bootEnvironment,
-    envMapIntensity: 2.0,
-  })
-  scene.children.forEach(mesh => {
-    mesh.material = material
-  })
-  const hemisphereLight = new THREE.HemisphereLight(skyColor, groundColor, hemisPhereIntensity)
-  const directionalLight = new THREE.DirectionalLight(directionalColor, directionalIntensity)
-  directionalLight.position.x = directionalLightPosition.x
-  directionalLight.position.y = directionalLightPosition.y
-  directionalLight.position.z = directionalLightPosition.z
-  scene.add(hemisphereLight)
-  scene.add(directionalLight)
-
-  const mixer = new THREE.AnimationMixer(scene)
-  const action = mixer.clipAction(scene.animations[0])
-  action.play()
-
-  return [scene, mixer]
-}
+import { createRenderer, startRendering, loadTexture, loadEnvironment } from './util'
 
 
 export default async () => {
-  const camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000)
+  const stage = await createRenderer({
+    scene: '/assets3d/bootSzene.json',
+    light: {
+      hemiSphere: {    
+        skyColor: 0xdde8fd,
+        groundColor: 0xd1daed,
+        intensity: 1.1
+      },
 
-  const renderer = new THREE.WebGLRenderer({ alpha: true })
-  renderer.setSize(window.innerWidth, window.innerWidth / 2)
-  document.body.querySelector('#boot').appendChild(renderer.domElement)
-
-  const [scene, mixer] = await createBootSzene()
-
-  let initialZ = 9.682811196058351
-
-  camera.position.x = 15 
-  camera.position.y = 4
-  camera.position.z = 15
-  camera.rotation.y += .8
-
-
-  const captureCamera = (e) => {
-    // allow horizontal angles
-    const alpha = 0.001
-    camera.rotation.y += alpha*e.movementX
-    // camera.rotation.x += alpha*e.movementY
-  }
-
-  document.addEventListener('mousedown', () => {
-    document.addEventListener('mousemove', captureCamera)
-  })
-  document.addEventListener('mouseup', () => {
-    document.removeEventListener('mousemove', captureCamera)
-  })
-
-  document.addEventListener('scroll', (e) => {
-    const alpha = 0.01
-    //camera.position.z = initialZ - alpha*window.scrollY
-  })
-
-
-  let deltaSeconds = 0
-  let lastTime = 0
-  const animate = function (time) {
-    if (lastTime) {
-      deltaSeconds = (time - lastTime) / 1000
-    } else {
-      deltaSeconds = 0
+      directional: {
+        color: 0xfaca8c,
+        intensity: 1.0,
+        position: {
+          x: 1,
+          y: 1,
+          z: -3
+        }
+      }
+    },
+    material: {
+      map: loadTexture('/assets3d/Atlas_color.png'),
+      aoMap: loadTexture('/assets3d/SchiffSzene_AO.jpg'),
+      roughnessMap: loadTexture('/assets3d/Atlas_roughness.png'),
+      roughness: 0.8,
+      aoMapIntensity: 1.2,
+      envMap: loadEnvironment('/assets3d/cubemaps/bluesky/'),
+      envMapIntensity: 2.0
+    },
+    domSelector: '#boot',
+    camera: {
+      position: {
+        x: 15,
+        y: 4,
+        z: 15
+      },
+      rotation: {
+        x: 0,
+        y: .8,
+        z: 0
+      }
     }
-    lastTime = time
-    mixer.update(deltaSeconds)
-    renderer.render(scene, camera)
-
-    requestAnimationFrame(animate)
-  }
-
-  animate()
+  })
+  startRendering(stage)
 }
+
---
+++ src/js/util.js
@@ -0,0 +1,107 @@
+import * as THREE from 'three'
+
+export const loadScene = async (name) => {
+  return new Promise(resolve => {
+    const loader = new THREE.ObjectLoader()
+    loader.load(name, scene => {
+      resolve(scene)
+    })
+  })
+}
+
+export const loadTexture = filename => new THREE.TextureLoader().load(filename)
+export const loadEnvironment = path => new THREE.CubeTextureLoader()
+  .setPath(path)
+  .load([
+    'posX.jpg',
+    'negX.jpg',
+    'posY.jpg',
+    'negY.jpg',
+    'posZ.jpg',
+    'negZ.jpg'
+  ])
+
+export const createHemisphereLight = config => new THREE.HemisphereLight(config.skyColor, config.groundColor, config.intensity)
+export const createDirectionalLight = config => {
+  const light = new THREE.DirectionalLight(config.color, config.intensity)
+  light.position.x = config.position.x
+  light.position.y = config.position.y
+  light.position.z = config.position.z
+  return light
+}
+
+export const createScene = async (config) => {
+  const scene = await loadScene(config.scene)
+  const material = new THREE.MeshStandardMaterial(config.material)
+  scene.children.forEach(mesh => {
+    mesh.material = material
+  })
+  scene.add(createHemisphereLight(config.light.hemiSphere))
+  scene.add(createDirectionalLight(config.light.directional))
+
+  const mixer = new THREE.AnimationMixer(scene)
+  const action = mixer.clipAction(scene.animations[0])
+  action.play()
+
+  return { scene, mixer }
+}
+
+export const createRenderer = async (config) => {
+  const camera = createCamera(config)
+  const renderer = new THREE.WebGLRenderer({ alpha: true })
+  const domElement = document.querySelector(config.domSelector)
+  renderer.setSize(domElement.clientWidth, domElement.clientHeight)
+  domElement.appendChild(renderer.domElement)
+
+  const { scene, mixer } = await createScene(config)
+
+  return { camera, scene, mixer, renderer }
+}
+
+export const createCamera = (config) => {
+  const domElement = document.querySelector(config.domSelector)
+  const camera = new THREE.PerspectiveCamera(45, domElement.clientWidth/domElement.clientHeight, 0.1, 1000)
+  camera.position.x = config.camera.position.x
+  camera.position.y = config.camera.position.y
+  camera.position.z = config.camera.position.z
+
+  camera.rotation.x += config.camera.rotation.x
+  camera.rotation.y += config.camera.rotation.y
+  camera.rotation.z += config.camera.rotation.z
+
+  return camera
+}
+
+export const startRendering = ({ camera, scene, mixer, renderer }) => {
+  let deltaSeconds = 0
+  let lastTime = 0
+  const animate = function (time) {
+    if (lastTime) {
+      deltaSeconds = (time - lastTime) / 1000
+    } else {
+      deltaSeconds = 0
+    }
+    lastTime = time
+    mixer.update(deltaSeconds)
+    renderer.render(scene, camera)
+
+    requestAnimationFrame(animate)
+  }
+
+  animate()  
+}
+
+export const enableMouseLook = (camera) => {
+  const captureCamera = (e) => {
+    // allow horizontal angles
+    const alpha = 0.001
+    camera.rotation.y += alpha*e.movementX
+  }
+
+  document.addEventListener('mousedown', () => {
+    document.addEventListener('mousemove', captureCamera)
+  })
+  document.addEventListener('mouseup', () => {
+    document.removeEventListener('mousemove', captureCamera)
+  })  
+}
