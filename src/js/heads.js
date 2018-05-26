c7c789f376977c2ebde1e718dfbb78045669d09a
Christoph Franke <krito@krito.de>
2018-05-26 22:27:22 +0200

    really load sequentially

--- src/js/heads.js
+++ src/js/heads.js
@@ -1,69 +1,69 @@
 import * as THREE from 'three'
 import { createRenderer, startRendering, loadTexture, loadEnvironment } from './util'
 
-const headsConfig = {
-  light: {
-    hemiSphere: {    
-      skyColor: 0xdde8fd,
-      groundColor: 0xe0e2ff,
-      intensity: 0.6
-    },
+export default async () => {
+  const headsConfig = {
+    light: {
+      hemiSphere: {    
+        skyColor: 0xdde8fd,
+        groundColor: 0xe0e2ff,
+        intensity: 0.6
+      },
 
-    directional: {
-      color: 0xf5f1e5,
-      intensity: 1.0,
+      directional: {
+        color: 0xf5f1e5,
+        intensity: 1.0,
+        position: {
+          x: 0.6,
+          y: 1,
+          z: 1
+        }
+      }
+    },
+    material: {
+      map: loadTexture('/assets3d/Atlas_color.png'),
+      roughnessMap: loadTexture('/assets3d/Atlas_roughness.png'),
+      roughness: 1.2,
+      aoMapIntensity: 2.0,
+      envMap: loadEnvironment('/assets3d/cubemaps/bluesky/'),
+      envMapIntensity: 2.0
+    },
+    domSelector: '#boot',
+    camera: {
+      fieldOfView: 45,
       position: {
-        x: 0.6,
-        y: 1,
-        z: 1
+        x: 0,
+        y: 0,
+        z: 1.5
+      },
+      rotation: {
+        x: 0,
+        y: 0,
+        z: 0
       }
     }
-  },
-  material: {
-    map: loadTexture('/assets3d/Atlas_color.png'),
-    roughnessMap: loadTexture('/assets3d/Atlas_roughness.png'),
-    roughness: 1.2,
-    aoMapIntensity: 2.0,
-    envMap: loadEnvironment('/assets3d/cubemaps/bluesky/'),
-    envMapIntensity: 2.0
-  },
-  domSelector: '#boot',
-  camera: {
-    fieldOfView: 45,
-    position: {
-      x: 0,
-      y: 0,
-      z: 1.5
-    },
-    rotation: {
-      x: 0,
-      y: 0,
-      z: 0
-    }
   }
-}
 
-const kritoConfig = {
-  ...headsConfig,
-  scene: '/assets3d/KopfKrito.json',
-  material: {
-    ...headsConfig.material,
-    aoMap: loadTexture('/assets3d/KopfKrito_AO.jpg'),
-  },
-  domSelector: '#krito'
-}
+  const kritoConfig = {
+    ...headsConfig,
+    scene: '/assets3d/KopfKrito.json',
+    material: {
+      ...headsConfig.material,
+      aoMap: loadTexture('/assets3d/KopfKrito_AO.jpg'),
+    },
+    domSelector: '#krito'
+  }
 
-const heizerConfig = {
-  ...headsConfig,
-  scene: '/assets3d/KopfHeizer.json',
-  material: {
-    ...headsConfig.material,
-    aoMap: loadTexture('/assets3d/KopfHeizer_AO.jpg'),
-  },
-  domSelector: '#heizer'
-}
+  const heizerConfig = {
+    ...headsConfig,
+    scene: '/assets3d/KopfHeizer.json',
+    material: {
+      ...headsConfig.material,
+      aoMap: loadTexture('/assets3d/KopfHeizer_AO.jpg'),
+    },
+    domSelector: '#heizer'
+  }
 
-export default async () => {
   const krito = await createRenderer(kritoConfig)
   const heizer = await createRenderer(heizerConfig)
   startRendering(krito)
