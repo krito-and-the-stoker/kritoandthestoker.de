326a81f0e1d9dab8759060cfa142e5af10ef264d
Christoph Franke <krito@krito.de>
2018-06-09 14:44:53 +0200

    removed navigation

--- src/js/main.js
+++ src/js/main.js
@@ -3,32 +3,7 @@ import heads from './heads.js'
 
 const toArray = nl => Array.prototype.slice.call(nl)
 
-const initializeNav = () => {
-	toArray(document.querySelectorAll('nav a')).forEach((a) => {
-		a.addEventListener('click', (e) => {
-			e.preventDefault()
-			document.querySelector(a.getAttribute('href')).scrollIntoView({ 
-			  behavior: 'smooth' 
-			});
-		})
-	})
-
-	const brand = document.querySelector('.brand')
-	const nav = document.querySelector('nav')
-	const boot = document.querySelector('#boot')
-	window.addEventListener('scroll', () => {
-		if (window.scrollY - 50 >= boot.getBoundingClientRect().bottom) {
-			brand.classList.add('invisible')
-			nav.classList.add('background')
-		} else {
-			brand.classList.remove('invisible')
-			nav.classList.remove('background')
-		}
-	})
-}
-
 window.onload = async () => {
-	initializeNav()
 	await boot()
 	await heads()
 }
